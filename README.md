d4a8e799001d942e4381061bbdaf2213f3086139
johnmontaguehowitt <jmhowitt@mac.com>
2020-03-10 13:35:15 +0000

    Added the test data and worked on the readme

--- README.md
+++ README.md
@@ -1,3 +1,38 @@
-# dummy
+# bo
 
-just created to get the readme and the unlicense
+## overview
+
+## Assumptions
+* i am assuming you are running on a mac with catalina 
+## database creation & importing the data
+* run the initdb - this will create directory instances
+* at the command line cd into instances - there will be a file bo.sqlite
+* run sqlite3 to get the prompt sqlite>
+* then run the following
+* sqlite> .open bo.sqlite
+* sqlite> .tables
+    * this will give you a list of the created tables
+* sqlite> .mode csv
+    * to tell sqlite you are importing csv files
+* sqlite> .import /path-to-working-dir/file-name.csv tablename
+    * on my system it looks like this
+    * sqlite> .import /Users/jmhowitt/PycharmProjects/suade/csvdata/promotions.csv promotions
+* do this for all six tables
+    * commissions
+    * orders
+    * products
+    * order_lines
+    * product_promotions
+    * promotions
+* n.b. this method imports all the line even if the first line are the column header. because of this with the sample data i have removed the column heads.
+* 
+* 
+
+## tests
+
+## how to use
+
+
+## if i had had more time/production environment
+• would have used sqlalchemy for obvious reasons and i would have used Alembic to manage  the tables development.
+• i would have written and import routine for the databases to import the csv's
--- bo/__init__.py
+++ bo/__init__.py
@@ -30,102 +30,105 @@ def create_app(test_config=None):
     db.init_app(app)
 
     # a simple page that says hello
-    @app.route('/bonjour')
+    @app.route('/bonjour', methods=['GET'])
     def bonjour():
-        return 'Bonjour, Matelot!'
+        if request.method == 'GET':
+            return 'Bonjour, Matelot!'
 
     # the endpoint to create the json
-    @app.route('/bo')
+    @app.route('/bo', methods=['GET'])
     def bo():
-        args = request.args
-        wdnt = args['date']
-        wd = args['date'] + "%"
-        tacrs_arr = []
-        db = get_db()
-        json_output = {}
-        cids = db.execute(
-            'SELECT COUNT(customer_id) AS cid FROM orders WHERE created_at LIKE "' + wd + '"'
-        ).fetchall()
-        for cid in cids:
-            json_output["customers"] = cid['cid']
+        if request.method == 'GET':
+            args = request.args
+            wdnt = args['date']  # working date - no time
+            wd = args['date'] + "%"  # working date - with time
+            tacrs_arr = []  # array for commissions and promotions
+            db = get_db()
+            json_output = {}
 
-        sumdas = db.execute(
-            'SELECT SUM(discounted_amount) AS sumda FROM order_lines '
-            'JOIN orders ON order_lines.order_id = orders.id '
-            'WHERE created_at LIKE "' + wd + '"'
-        ).fetchall()
-        for sumda in sumdas:
-            json_output["total_discount_amount"] = sumda['sumda']
+            cids = db.execute(
+                'SELECT COUNT(customer_id) AS cid FROM orders WHERE created_at LIKE "' + wd + '"'
+            ).fetchall()
+            for cid in cids:
+                json_output["customers"] = cid['cid']
 
-
-        cpids = db.execute(
-            'SELECT COUNT(product_id) AS cpid FROM order_lines '
-            'JOIN orders ON order_lines.order_id = orders.id '
-            'WHERE created_at LIKE "' + wd + '"'
-        ).fetchall()
-        for cpid in cpids:
-            json_output["items"] = cpid['cpid']
+            sumdas = db.execute(
+                'SELECT SUM(discounted_amount) AS sumda FROM order_lines '
+                'JOIN orders ON order_lines.order_id = orders.id '
+                'WHERE created_at LIKE "' + wd + '"'
+            ).fetchall()
+            for sumda in sumdas:
+                json_output["total_discount_amount"] = sumda['sumda']
 
 
-        avgtas = db.execute(
-            'SELECT AVG(total_amount) AS avgta FROM order_lines '
-            'JOIN orders ON order_lines.order_id = orders.id '
-            'WHERE created_at LIKE "' + wd + '"'
-        ).fetchall()
-        for avgta in avgtas:
-            json_output["order_total_avg"] = avgta['avgta']
+            cpids = db.execute(
+                'SELECT COUNT(product_id) AS cpid FROM order_lines '
+                'JOIN orders ON order_lines.order_id = orders.id '
+                'WHERE created_at LIKE "' + wd + '"'
+            ).fetchall()
+            for cpid in cpids:
+                json_output["items"] = cpid['cpid']
 
 
-        avgdrs = db.execute(
-            'SELECT AVG(discount_rate) AS avgdr FROM order_lines '
-            'JOIN orders ON order_lines.order_id = orders.id '
-            'WHERE created_at LIKE "' + wd + '"'
-        ).fetchall()
-        for avgdr in avgdrs:
-            json_output["discount_rate_avg"] = avgdr['avgdr']
+            avgtas = db.execute(
+                'SELECT AVG(total_amount) AS avgta FROM order_lines '
+                'JOIN orders ON order_lines.order_id = orders.id '
+                'WHERE created_at LIKE "' + wd + '"'
+            ).fetchall()
+            for avgta in avgtas:
+                json_output["order_total_avg"] = avgta['avgta']
 
 
-        tacrs = db.execute(
-            'SELECT order_lines.product_id, order_lines.total_amount, product_promotions.promotion_id, '
-            'SUM(order_lines.total_amount * commissions.rate) AS tacr FROM orders JOIN order_lines '
-            'ON orders.id = order_lines.order_id LEFT JOIN product_promotions '
-            'ON order_lines.product_id = product_promotions.product_id LEFT JOIN commissions '
-            'ON orders.vendor_id = commissions.vendor_id WHERE created_at LIKE "' + wd + '"'
-            ' AND product_promotions.promotion_id IS NOT null '
-            'AND commissions.date = "' + wdnt + '" '
-            'AND product_promotions.date = "2019-08-12" GROUP BY product_promotions.promotion_id'
-        ).fetchall()
-        comm_json = {}
-        promo_json = {}
-        cp_counter = 1
-        for tacr in tacrs:
-            promo_json[cp_counter] = tacr['tacr']
-            cp_counter += 1
-            tacrs_arr.append(tacr['tacr'])
+            avgdrs = db.execute(
+                'SELECT AVG(discount_rate) AS avgdr FROM order_lines '
+                'JOIN orders ON order_lines.order_id = orders.id '
+                'WHERE created_at LIKE "' + wd + '"'
+            ).fetchall()
+            for avgdr in avgdrs:
+                json_output["discount_rate_avg"] = avgdr['avgdr']
 
-        comm_json["promotions"] = promo_json
-        json_output["commissions"] = comm_json
 
-        sumtars = db.execute(
-            'SELECT SUM(order_lines.total_amount * commissions.rate) AS sumtar '
-            'FROM orders JOIN order_lines ON orders.id = order_lines.order_id '
-            'JOIN commissions on orders.vendor_id = commissions.vendor_id '
-            'WHERE created_at LIKE "' + wd + '" AND commissions.date = "' + wdnt + '"'
-        ).fetchall()
-        for sumtar in sumtars:
-            json_output["total"] = sumtar['sumtar']
+            tacrs = db.execute(
+                'SELECT order_lines.product_id, order_lines.total_amount, product_promotions.promotion_id, '
+                'SUM(order_lines.total_amount * commissions.rate) AS tacr FROM orders JOIN order_lines '
+                'ON orders.id = order_lines.order_id LEFT JOIN product_promotions '
+                'ON order_lines.product_id = product_promotions.product_id LEFT JOIN commissions '
+                'ON orders.vendor_id = commissions.vendor_id WHERE created_at LIKE "' + wd + '"'
+                ' AND product_promotions.promotion_id IS NOT null '
+                'AND commissions.date = "' + wdnt + '" '
+                'AND product_promotions.date = "2019-08-12" GROUP BY product_promotions.promotion_id'
+            ).fetchall()
+            comm_json = {}
+            promo_json = {}
+            cp_counter = 1
+            for tacr in tacrs:
+                promo_json[cp_counter] = tacr['tacr']
+                cp_counter += 1
+                tacrs_arr.append(tacr['tacr'])
 
-        averageperorders = db.execute(
-            'SELECT orders.id AS orderid, order_lines.product_id, order_lines.total_amount, '
-            'commissions.rate, AVG(order_lines.total_amount * commissions.rate) as averageperorder '
-            'FROM orders LEFT JOIN order_lines ON orders.id = order_lines.order_id '
-            'LEFT JOIN commissions on orders.vendor_id = commissions.vendor_id '
-            'WHERE created_at LIKE "' + wd + '" AND commissions.date = "' + wdnt + '"'
-        ).fetchall()
-        for averageperorder in averageperorders:
-            json_output["order_average"] = averageperorder['averageperorder']
+            comm_json["promotions"] = promo_json
+            json_output["commissions"] = comm_json
 
-        json_string = json.dumps(json_output)
-        return json_string
+            sumtars = db.execute(
+                'SELECT SUM(order_lines.total_amount * commissions.rate) AS sumtar '
+                'FROM orders JOIN order_lines ON orders.id = order_lines.order_id '
+                'JOIN commissions on orders.vendor_id = commissions.vendor_id '
+                'WHERE created_at LIKE "' + wd + '" AND commissions.date = "' + wdnt + '"'
+            ).fetchall()
+            for sumtar in sumtars:
+                json_output["total"] = sumtar['sumtar']
+
+            averageperorders = db.execute(
+                'SELECT orders.id AS orderid, order_lines.product_id, order_lines.total_amount, '
+                'commissions.rate, AVG(order_lines.total_amount * commissions.rate) as averageperorder '
+                'FROM orders LEFT JOIN order_lines ON orders.id = order_lines.order_id '
+                'LEFT JOIN commissions on orders.vendor_id = commissions.vendor_id '
+                'WHERE created_at LIKE "' + wd + '" AND commissions.date = "' + wdnt + '"'
+            ).fetchall()
+            for averageperorder in averageperorders:
+                json_output["order_average"] = averageperorder['averageperorder']
+
+            json_string = json.dumps(json_output)
+            return json_string
 
     return app
